6d804dfad6bdb0d1d86823751120f688ad10cb81
nilenth <nilenth92@gmail.com>
2018-08-08 12:28:21 +0530

    Merge branch 'master' of https://github.com/chathurabr/ancon
    # Conflicts:
    #	src/main/java/com/ancon/automation/pages/Tenants.java
    #	src/main/java/com/ancon/automation/tests/TenantsTest.java

--- src/main/java/com/ancon/automation/pages/Tenants.java
+++ src/main/java/com/ancon/automation/pages/Tenants.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-import java.sql.Driver;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
