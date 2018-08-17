a4a7e4de3cdf4435781f8187cc7d7a1cd9670958
chathurabr <chathura.br@gmail.com>
2018-08-17 09:34:06 +0530

    test change

--- src/main/java/com/ancon/automation/tests/TenantsTest.java
+++ src/main/java/com/ancon/automation/tests/TenantsTest.java
@@ -55,10 +55,10 @@ public class TenantsTest {
                 .addSystemInfo("Environment", "Ancon Automation Testing")
                 .addSystemInfo("User Name", "Chathura");
        // extent.loadConfig(new File(System.getProperty("user.dir") + CommonClass.path + "utils\\extent-config.xml"));
-/*    }
+    }
 
     @BeforeClass
-    public void SetUp() throws IOException {*/
+    public void SetUp() throws IOException {
         driver = DriverFactory.getDriver();
         commonClass = new CommonClass(driver);
         login = new Login(driver);
@@ -69,11 +69,7 @@ public class TenantsTest {
         // get data from property file
         Properties properties = new Properties();
         String filePath = System.getProperty("user.dir");
-        try {
-            properties.load(new FileInputStream(".\\Base.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        properties.load(new FileInputStream(".\\Base.properties"));
         email = properties.getProperty("email");
         password = properties.getProperty("password");
         //Tenant Details
